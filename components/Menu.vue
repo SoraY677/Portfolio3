6df328b9aa85e82eb88a683b56ef4753c354be2b
SoraY677 <ys4080@icloud.com>
2020-11-10 12:52:08 +0900

    [update]Menuのアニメーションを作成

---
+++ components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="l-square">
+  <div class="l-square-content">
+   <button @click="$emit('click')">
+    <div class="bar-container">
+     <span class="bar"></span>
+    </div>
+   </button>
+  </div>
+ </div>
+</template>
+
+<style scope>
+button {
+ width: 100%;
+ height: 100%;
+ padding:15%;
+ border-radius: 50%;
+}
+
+.bar-container{
+	position: relative;
+	width:100%;
+	height:100%;
+}
+
+.bar,
+.bar::before {
+ position: absolute;
+ top: 50%;
+ left: 0;
+ width: 100%;
+ background-color: #2e2e2e;
+}
+.bar {
+ transform: translateY(-50%);
+ height: 10%;
+}
+
+.bar::before {
+ content: "";
+ height: 100%;
+ transform: translateY(-50%) rotate(90deg);
+}
+</style>
---
+++ layouts/LMenu.vue
@@ -0,0 +1,44 @@
+<template>
+ <nav :class="{ active: isActive }">
+  <Menu
+   @click="isActive = !isActive"
+   class="menu-btn"
+   :class="{ active: isActive }"
+  />
+ </nav>
+</template>
+
+<script>
+import Menu from "~/components/Menu.vue";
+export default {
+ data() {
+  return {
+   isActive: false,
+  };
+ },
+ components: {
+  Menu,
+ },
+};
+</script>
+
+<style scope>
+nav {
+ position: fixed;
+ width: 50px;
+ height: 100%;
+}
+
+nav,
+.menu-btn {
+ transition-duration: 0.5s;
+}
+
+nav.active {
+ transform: translateY(50%);
+}
+
+.menu-btn.active {
+ transform: rotate(45deg);
+}
+</style>
