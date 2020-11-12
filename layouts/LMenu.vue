605d8321fa307369a644defb8957d96dff508213
SoraY677 <ys4080@icloud.com>
2020-11-12 10:12:33 +0900

    メニュー制作

--- layouts/LMenu.vue
+++ layouts/LMenu.vue
@@ -1,23 +1,28 @@
 <template>
- <nav :class="{ active: isActive }">
-  <Menu
-   @click="isActive = !isActive"
-   class="menu-btn"
-   :class="{ active: isActive }"
-  />
-  <ul>
-   <li
-    v-for="item in linkList"
-    :key="item.id"
-    class="menu-item"
-    :class="{ active: isActive }"
-   >
-    <nuxt-link :to="item">
-     {{ item }}
-    </nuxt-link>
-   </li>
-  </ul>
- </nav>
+ <div>
+  <div class="overlay" :class="{ active: isActive }"></div>
+  <nav :class="{ active: isActive }">
+   <div class="menu-btn-container">
+    <Menu
+     @click="isActive = !isActive"
+     class="menu-btn"
+     :class="{ active: isActive }"
+    />
+   </div>
+   <ul class="menu-list">
+    <li
+     v-for="item in linkList"
+     :key="item.id"
+     class="menu-item"
+     :class="{ active: isActive }"
+    >
+     <nuxt-link :to="item">
+      {{ item }}
+     </nuxt-link>
+    </li>
+   </ul>
+  </nav>
+ </div>
 </template>
 
 <script>
@@ -36,12 +41,30 @@ export default {
 </script>
 
 <style scoped>
+.overlay {
+ position: fixed;
+ z-index: 99;
+ width: 100%;
+ height: 100%;
+ transform: translateX(-100%);
+ transition: 0.2s;
+ background-color: #222;
+}
+
+.overlay.active {
+ transform: translateX(0);
+}
+
 nav {
  position: fixed;
+ display: flex;
+ z-index: 100;
  left: 10px;
- width: 50px;
- height: 100%;
  transform: translateY(10px);
+}
+
+.menu-btn-container {
+ width: 50px;
 }
 
 nav,
@@ -50,22 +73,34 @@ nav,
 }
 
 nav.active {
- transform: translateY(50%);
+ transform: translateY(50vh);
 }
 
 .menu-btn.active {
  transform: rotate(135deg);
 }
 
+.menu-list {
+ color: white;
+ font-size: 3rem;
+ padding-left: 20px;
+ transform: translateY(-25%);
+}
+
 .menu-item {
- opacity: 0;
+ width: 0%;
+ overflow: hidden;
  transition-delay: 0s;
  transition-duration: 0s;
 }
 
 .menu-item.active {
- opacity: 1;
+ width: 100%;
  transition-delay: 0.5s;
  transition-duration: 0.5s;
+}
+
+.menu-item > a {
+ color: white;
 }
 </style>
