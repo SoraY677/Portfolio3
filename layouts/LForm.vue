9f9a91878f1f44d14e4b6a478a63879ee47ffc20
SoraY677 <ys4080@icloud.com>
2020-11-12 09:36:39 +0900

    ページ名が勝手に表示されるようにした

--- layouts/LForm.vue
+++ layouts/LForm.vue
@@ -1,15 +1,26 @@
 <template>
  <div class="base-frame">
   <Menu />
+  <PageName :name="pageName" />
   <nuxt />
  </div>
 </template>
 
 <script>
+import PageName from "@/components/PageName";
 import Menu from "~/layouts/LMenu.vue";
 export default {
+ data() {
+  return {
+   pageName: "",
+  };
+ },
  components: {
   Menu,
+  PageName,
+ },
+ mounted() {
+  this.pageName = this.$route.name;
  },
 };
 </script>
--- pages/About.vue
+++ pages/About.vue
@@ -1,16 +1,11 @@
 <template>
- <div>
-  <PageName name="About" />
- </div>
+ <div></div>
 </template>
 
 <script>
-import PageName from "@/components/PageName";
 export default {
  layout: "LForm",
- components: {
-  PageName,
- },
+ components: {},
 };
 </script>
 
