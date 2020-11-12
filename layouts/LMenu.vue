3b1965e932e429b6259632e43cb78c0128abcd0f
SoraY677 <ys4080@icloud.com>
2020-11-13 00:33:30 +0900

    [fix]リンクが変なのを修正

--- layouts/LMenu.vue
+++ layouts/LMenu.vue
@@ -1,113 +1,113 @@
 <template>
- <div ref="test">
-  <div class="overlay" :class="{ active: isActive }"></div>
-  <nav :class="{ active: isActive }">
-   <div class="menu-btn-container">
-    <Menu
-     @click="isActive = !isActive"
-     class="menu-btn"
-     :class="{ active: isActive }"
-     :isActive="isActive"
-    />
-   </div>
-   <ul class="menu-list">
-    <li
-     v-for="item in linkList"
-     :key="item.id"
-     class="menu-item"
-     :class="{ active: isActive }"
-    >
-     <nuxt-link @click.native="closeMenu()" :to="item">
-      {{ item }}
-     </nuxt-link>
-    </li>
-   </ul>
-  </nav>
- </div>
+  <div ref="test">
+    <div class="overlay" :class="{ active: isActive }"></div>
+    <nav :class="{ active: isActive }">
+      <div class="menu-btn-container">
+        <Menu
+          @click="isActive = !isActive"
+          class="menu-btn"
+          :class="{ active: isActive }"
+          :isActive="isActive"
+        />
+      </div>
+      <ul class="menu-list">
+        <li
+          v-for="item in linkList"
+          :key="item.id"
+          class="menu-item"
+          :class="{ active: isActive }"
+        >
+          <nuxt-link @click.native="closeMenu()" :to="'/' + item">
+            {{ item }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
 import Menu from "~/components/Menu.vue";
 export default {
- data() {
-  return {
-   isActive: false,
-   linkList: ["About", "Work", "Blog", "Contact"],
-  };
- },
- components: {
-  Menu,
- },
- methods: {
-  //メニューを閉じる
-  closeMenu() {
-   this.isActive = false;
+  data() {
+    return {
+      isActive: false,
+      linkList: ["About", "Work", "Blog", "Contact"],
+    };
   },
- },
+  components: {
+    Menu,
+  },
+  methods: {
+    //メニューを閉じる
+    closeMenu() {
+      this.isActive = false;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .overlay {
- position: fixed;
- z-index: 99;
- width: 100%;
- height: 100%;
- transform: translateX(-100%);
- transition: 0.2s;
- background-color: #222;
+  position: fixed;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  transform: translateX(-100%);
+  transition: 0.2s;
+  background-color: #222;
 }
 
 .overlay.active {
- transform: translateX(0);
+  transform: translateX(0);
 }
 
 nav {
- position: fixed;
- display: flex;
- z-index: 100;
- left: 10px;
- transform: translateY(10px);
+  position: fixed;
+  display: flex;
+  z-index: 100;
+  left: 10px;
+  transform: translateY(10px);
 }
 
 .menu-btn-container {
- width: 50px;
+  width: 50px;
 }
 
 nav,
 .menu-btn {
- transition-duration: 0.5s;
+  transition-duration: 0.5s;
 }
 
 nav.active {
- transform: translateY(50vh);
+  transform: translateY(50vh);
 }
 
 .menu-btn.active {
- transform: rotate(135deg);
+  transform: rotate(135deg);
 }
 
 .menu-list {
- color: white;
- font-size: 3rem;
- padding-left: 20px;
- transform: translateY(-25%);
+  color: white;
+  font-size: 3rem;
+  padding-left: 20px;
+  transform: translateY(-25%);
 }
 
 .menu-item {
- width: 0%;
- overflow: hidden;
- transition-delay: 0s;
- transition-duration: 0s;
+  width: 0%;
+  overflow: hidden;
+  transition-delay: 0s;
+  transition-duration: 0s;
 }
 
 .menu-item.active {
- width: 100%;
- transition-delay: 0.5s;
- transition-duration: 0.5s;
+  width: 100%;
+  transition-delay: 0.5s;
+  transition-duration: 0.5s;
 }
 
 .menu-item > a {
- color: white;
+  color: white;
 }
 </style>
